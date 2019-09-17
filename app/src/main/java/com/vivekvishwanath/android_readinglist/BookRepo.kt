4b9eb81138e79afe4e189c368a4c3632933f9a31
VivekV95 <vivekv210@gmail.com>
2019-09-17 14:20:10 -0500

    Refactor project to Kotlin

---
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/BookRepo.kt
@@ -0,0 +1,29 @@
+package com.vivekvishwanath.android_readinglist
+
+import java.util.ArrayList
+
+object BookRepo {
+
+    val bookArray: Array<Book?>
+        get() {
+            val books = arrayOfNulls<Book>(Integer.parseInt(SharedPrefsDao.nextId))
+            for (i in books.indices) {
+                val bookCsv = SharedPrefsDao.getBookCsv(i.toString())
+                books[i] = bookCsv?.let { Book(it) }
+            }
+            return books
+        }
+
+    fun getBook(id: Int): Book {
+        return Book(SharedPrefsDao.getBookCsv(Integer.toString(id))!!)
+    }
+
+    fun nextId(): Int {
+        return Integer.parseInt(SharedPrefsDao.nextId)
+    }
+
+    fun updateBook(book: Book) {
+        SharedPrefsDao.updateBook(book)
+    }
+
+}
---
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/EditBookActivity.kt
@@ -0,0 +1,66 @@
+package com.vivekvishwanath.android_readinglist
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_edit_book.*
+
+class EditBookActivity : AppCompatActivity() {
+    private var context: Context? = null
+    private var id: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_book)
+        context = this
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        save_button.setOnClickListener { returnData() }
+
+        cancel_button.setOnClickListener { cancel() }
+
+        val intent = intent
+        id = intent.getStringExtra(Constants.NEW_BOOK_TAG)
+        val bookCsv = intent.getStringExtra(Constants.EDIT_BOOK_TAG)
+        if (bookCsv != null) {
+            val book = Book(bookCsv)
+            book_name_text.setText(book.title)
+            book_reason_text.setText(book.reasonToRead)
+            read_switch.isChecked = book.isHasBeenRead
+            id = book.id
+        }
+    }
+
+    private fun returnData() {
+        val bookName = book_name_text.text.toString()
+        val bookReason = book_reason_text.text.toString()
+        val switchStatus = read_switch.isChecked
+        val book = Book(id!!, bookName, bookReason, read_switch.isChecked)
+        val bookCsv = book.toCsvString()
+        val intent = Intent()
+        intent.putExtra(Constants.EDIT_BOOK_TAG, bookCsv)
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
+    private fun cancel() {
+        val intent = Intent()
+        setResult(RESULT_CANCELED, intent)
+        finish()
+    }
+
+    override fun onBackPressed() {
+        returnData()
+    }
+}
